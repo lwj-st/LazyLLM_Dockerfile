00e9b6ac60739ff662a1fca4ed149f3f34e6f82f
lwj-st <liwenjian.vendor@sensetime.com>
2025-03-13 10:53:05 +0800

    支持vllm0.7.3，个人镜像使用本地环境

--- User/build.sh
+++ User/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-version=cuda12.1.0-cudnn8-ubuntu22.04_250224
+#version=cuda12.1.0-cudnn8-ubuntu22.04_250224
 
 export Server_Dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 cd $Server_Dir
 for i in `ls |grep ^Dockerfile_`;
 do
   echo $i
-  user=${i#*_}
-  echo $user
-  docker build --build-arg version=$version -f $i -t registry.cn-sh-01.sensecore.cn/ai-expert-service/lazyllm:${version}_${user} .
+  image_tag=${i#*_}
+  echo $image_tag
+  docker build  -f $i -t registry.cn-sh-01.sensecore.cn/ai-expert-service/lazyllm:${image_tag} .
 done
